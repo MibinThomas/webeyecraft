476fd6c7cc8559c899e5b621e4cb2f5b1d5747ae
Athul Chandran <m.athulchandran@gmail.com>
2024-11-28 07:29:47 +0530

    Initial Stage commit, Basic SEO Optimisation, and floating button to whatsapp chat added and updated.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import SEO from "../common/seo";
 import HomeOne from "../components/homes/home";
 import Wrapper from "../layout/wrapper";
@@ -5,8 +6,25 @@ import Wrapper from "../layout/wrapper";
 const Home = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={"WEBEYECRAFT TECHNOLOGIES PVT LTD"} />
+      <SEO pageTitle={"Webeyecraft"} />
       <HomeOne />
+
+      <div className="floating-btn-wrapper">
+        <Link
+          href="https://api.whatsapp.com/send?phone=+917994394866&text=Hello%2C%20I%20want%20to%20know%20more%20about%20Webeyecraft%20Technologies."
+          className="tp-btn-inner"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Connect through Whatsapp"
+        >
+          <img
+            src="/assets/img/whatsapp.png"
+            alt=""
+            width="60px"
+            height="auto"
+          />
+        </Link>
+      </div>
     </Wrapper>
   );
 };
